6e8b7a6787d02e956b950015f63c293d1e7f54b9
Li Xinhang <xinhangl44@gmail.com>
2026-04-14 21:53:16 +0800

    Update app_file_uploader.py

--- app_file_uploader.py
+++ app_file_uploader.py
@@ -1,13 +1,11 @@
 #app_file_uploader
 import streamlit as st
 from knowledge_base import KnowledgeBaseService
-#添加标题
-st.title("知识库更新服务")
-#文件上传服务
+st.title("data bases updating service")
 uploader_file=st.file_uploader(
     "上传txt文件",
     type=['txt'],
-    accept_multiple_files=False,#仅接受上传一个文件
+    accept_multiple_files=False,
 )
 if "service" not in st.session_state:
     st.session_state["service"]=KnowledgeBaseService()
@@ -23,7 +21,6 @@ if uploader_file is not None:
     st.subheader(f"{file_name}")
     st.write(f"格式:{file_type},大小:{file_size:.2f}")
 
-#获取内容
     text=uploader_file.getvalue().decode("utf-8")
 
     st.session_state["counter"]+=1
